85aae9258e9b62092e5a9de2a0548e74a8583fe7
pedrovitor004 <pedrovitorbarbosaflorentino@gmail.com>
2026-04-26 22:19:59 -0300

    feat: update OpenAPI configuration to include custom info

---
+++ src/main/java/org/example/config/OpenApiConfig.java
@@ -0,0 +1,44 @@
+package org.example.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Value("${app.name}")
+    private String appName;
+
+    @Value("${app.description}")
+    private String appDescription;
+
+    @Value("${app.version}")
+    private String appVersion;
+
+    @Value("${app.author}")
+    private String author;
+
+    @Value("${app.email}")
+    private String email;
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(appName)
+                        .version(appVersion)
+                        .description(appDescription)
+                        .contact(new Contact()
+                                .name(author)
+                                .email(email)
+                                .url("https://github.com/Lara-Menezes/projeto-dac-backend-finalizatcc"))
+                        .license(new License()
+                                .name("MIT")
+                                .url("https://opensource.org/licenses/MIT")));
+    }
+}
